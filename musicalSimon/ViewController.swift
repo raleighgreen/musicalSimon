fb1fa7cebd165d59c0289d75fa7f9055202b86ab
Raleigh Green <raleighgreen@gmail.com>
2017-06-09 12:49:18 -0400

    create black background and flipped default status bar style

--- musicalSimon/ViewController.swift
+++ musicalSimon/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return.lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
